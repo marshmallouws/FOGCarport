10f2518a695cb1120127ade31721b3a2b0383b11
Martin <cph-md273@cphbusiness.dk>
2019-04-25 13:01:23 +0200

    OrderInfo implementation

--- src/main/java/data/Connector.java
+++ src/main/java/data/Connector.java
@@ -9,7 +9,7 @@ public class Connector {
 
     private static final String URL = "jdbc:mysql://localhost:3306/carports";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Repsac270390";
+    private static final String PASSWORD = "password";
 
     private static Connection singleton;
 
