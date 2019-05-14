ef5aad88fe95058d07a0e8a0c0b5b5ec402dca53
Casper Olesen <casperolesen@me.com>
2019-05-14 20:34:51 +0200

    Cleaned up ProductMapper and -Interface

--- src/main/java/data/ProductDAOInterface.java
+++ src/main/java/data/ProductDAOInterface.java
@@ -8,16 +8,20 @@ package data;
 import entity.Category;
 import entity.Product;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Annika
  */
 public interface ProductDAOInterface {
-    public void insertNewProduct(Product product, Category category);
-    public ArrayList<Product> getProducts();
-    public ArrayList<Product> getProducts(Category category);
+    public Product getProductVariant(int id);
     public Product getProductMain(int id);
-    public void setActivity(Product product);
-    public ArrayList<Product> getProducts(boolean activity);
+    public ArrayList<Category> getCategories();
+    public ArrayList<Product> getProductVariantsList(int categoryID, int productID);
+    public List<Product> getProductsInCategories(int categoryID);
+    public int createProductVariant(Product product);
+    public int createProduct(Product product);
+    public boolean updateProductVariant(Product product);
+    public List<Product> getRoofTypes();
 }
--- src/main/java/data/ProductMapper.java
+++ src/main/java/data/ProductMapper.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
-import com.sun.org.apache.bcel.internal.generic.Select;
 import entity.Category;
 import entity.Product;
 import java.sql.Connection;
@@ -15,9 +9,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.annotation.Generated;
 
 /**
  *
@@ -40,6 +31,7 @@ public class ProductMapper implements ProductDAOInterface {
         return new Product(id, variant_id, category, thickness, width, length, price, stock, name, active);
     }
 
+    @Override
     public Product getProductVariant(int id) {
         Product product = null;
         try {
@@ -64,65 +56,6 @@ public class ProductMapper implements ProductDAOInterface {
     }
 
     @Override
-    public void insertNewProduct(Product product, Category category) {
-        try {
-            Connection con = Connector.connection();
-            String query = "INSERT INTO product (cat_id, height, length, width, price, stock) "
-                    + "VALUES (?, ?, ?, ?, ?, ?)";
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setInt(1, category.getId());
-            ps.setInt(2, product.getHeight());
-            ps.setInt(3, product.getLength());
-            ps.setInt(4, product.getWidth());
-            ps.setDouble(5, product.getPrice());
-            ps.setInt(6, product.getStock());
-            ps.executeUpdate();
-
-        } catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    @Override
-    public ArrayList<Product> getProducts() {
-        ArrayList<Product> p = new ArrayList<>();
-        try {
-            Connection con = Connector.connection();
-            String query = "SELECT * FROM product;"; // OLD
-            PreparedStatement ps = con.prepareStatement(query);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                p.add(buildProductVariant(rs));
-            }
-        } catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return p;
-    }
-
-    private Category getCategory(int id) {
-        Category cat = null;
-        try {
-            Connection con = Connector.connection();
-            String query = "SELECT * FROM categories WHERE id = ?;"; // OLD
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                String name = rs.getString("cat_name");
-
-                cat = new Category(id, name, true, true, true); // OLD
-            }
-
-        } catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return cat;
-    }
-
-    @Override
     public Product getProductMain(int id) {
         Product product = null;
         try {
@@ -143,58 +76,9 @@ public class ProductMapper implements ProductDAOInterface {
         return product;
     }
 
-    @Override
-    public ArrayList<Product> getProducts(Category category) {
-        ArrayList<Product> p = new ArrayList<>();
-        try {
-            Connection con = Connector.connection();
-            String query = "SELECT * FROM product WHERE cat_id = ?;"; // OLD
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setInt(1, category.getId());
-            ResultSet rs = ps.executeQuery();
 
-            while (rs.next()) {
-                p.add(buildProductVariant(rs));
-            }
-        } catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return p;
-    }
 
     @Override
-    public void setActivity(Product product) {
-        try {
-            Connection con = Connector.connection();
-            String query = "UPDATE product SET active = ? WHERE id = ?;";
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setBoolean(1, !(product.isActive()));
-            ps.setInt(2, product.getId());
-            ps.executeUpdate();
-        } catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    @Override
-    public ArrayList<Product> getProducts(boolean activity) {
-        ArrayList<Product> p = new ArrayList<>();
-        try {
-            Connection con = Connector.connection();
-            String query = "SELECT * FROM product WHERE active = ?;"; // OLD
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setBoolean(1, activity);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                p.add(buildProductVariant(rs));
-            }
-        } catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return p;
-    }
-
     public ArrayList<Category> getCategories() {
         ArrayList<Category> cat = new ArrayList();
         try {
@@ -219,6 +103,7 @@ public class ProductMapper implements ProductDAOInterface {
         return cat;
     }
 
+    @Override
     public ArrayList<Product> getProductVariantsList(int categoryID, int productID) {
         ArrayList<Product> prod = new ArrayList();
         try {
@@ -243,6 +128,7 @@ public class ProductMapper implements ProductDAOInterface {
         return prod;
     }
 
+    @Override
     public List<Product> getProductsInCategories(int categoryID) {
         List<Product> products = new ArrayList();
         try {
@@ -266,6 +152,7 @@ public class ProductMapper implements ProductDAOInterface {
         return products;
     }
 
+    @Override
     public int createProductVariant(Product product) {
         int variantID = 0;
         try {
@@ -290,6 +177,7 @@ public class ProductMapper implements ProductDAOInterface {
         }
     }
 
+    @Override
     public int createProduct(Product product) {
         Connection con = null;
         try {
@@ -329,6 +217,7 @@ public class ProductMapper implements ProductDAOInterface {
         }
     }
 
+    @Override
     public boolean updateProductVariant(Product product) {
         try {
             Connection con = Connector.connection();
@@ -347,6 +236,7 @@ public class ProductMapper implements ProductDAOInterface {
         }
     }
 
+    @Override
     public List<Product> getRoofTypes() {
         List<Product> roofs = new ArrayList();
 
